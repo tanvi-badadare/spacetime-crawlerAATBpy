a2dce4959c7b2fba87cd55cf5c0c60c0e2306aa2
Tanvi Virendra Badadare <tanvi.badadare@gmail.com>
2026-02-04 22:54:46 -0800

    Update THREADCOUNT assert message

--- utils/config.py
+++ utils/config.py
@@ -8,7 +8,7 @@ class Config(object):
         assert self.user_agent != "DEFAULT AGENT", "Set useragent in config.ini"
         assert re.match(r"^[a-zA-Z0-9_ ,]+$", self.user_agent), "User agent should not have any special characters outside '_', ',' and 'space'"
         self.threads_count = int(config["LOCAL PROPERTIES"]["THREADCOUNT"])
-        assert 1 <= self.threads_count <= 4, "THREADCOUNT must be between 1 and 4 (per assignment)."
+        assert 1 <= self.threads_count <= 4, "THREADCOUNT must be between 1 and 4"
         self.save_file = config["LOCAL PROPERTIES"]["SAVE"]
 
         self.host = config["CONNECTION"]["HOST"]
